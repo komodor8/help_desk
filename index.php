79810f424ca3f756b73fe83896b60588acb0364b
= <=>
2026-04-26 16:48:41 +0200

    minor fix

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once 'includes/auth.php';
 startSession();
 
 if (isset($_SESSION['user_id'])) {
-    header('Location: dashboard.php');
+    header('Location: tickets/dashboard.php');
 } else {
     header('Location: login.php');
 }
--- login.php
+++ login.php
@@ -3,7 +3,7 @@ require_once 'includes/auth.php';
 startSession();
 
 if (isset($_SESSION['user_id'])) {
-    header('Location: /pages/dashboard.php');
+    header('Location: /tickets/dashboard.php');
     exit();
 }
 
